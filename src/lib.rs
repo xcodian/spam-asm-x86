58cc88dbd2c1e58cd247a0bcecb097c719ee8c25
MindMayhem <vps.sayder@gmail.com>
2023-05-26 13:27:21 +0300

    [fix] no_std support (std -> core)

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,11 @@ use proc_macro::TokenStream;
 use rand::{seq::SliceRandom, Rng};
 use syn::{Expr, Item, Stmt};
 
-const SAFE_REGS_32: &[&str] = &[
-    "eax", "ebx", "ecx", "edx", 
-    "edi", "ebp"
-]; // esi isn't good
-const SAFE_REGS_8: &[&str] = &[
-    "ah", "bh", "ch", "dh", 
-    "al", "bl", "cl", "dl"
-];
+const SAFE_REGS_32: &[&str] = &["eax", "ebx", "ecx", "edx", "edi", "ebp"]; // esi isn't good
+const SAFE_REGS_8: &[&str] = &["ah", "bh", "ch", "dh", "al", "bl", "cl", "dl"];
 const SAFE_REGS_MM: &[&str] = &[
-     "mm0",  "mm1",  "mm2",  "mm3",  "mm4",  "mm5",  "mm6",  "mm7", 
-    "xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7",
+    "mm0", "mm1", "mm2", "mm3", "mm4", "mm5", "mm6", "mm7", "xmm0", "xmm1", "xmm2", "xmm3", "xmm4",
+    "xmm5", "xmm6", "xmm7",
 ];
 
 const OPS_ALL: &[&str] = &[
@@ -67,7 +61,7 @@ fn get_useless_block() -> TokenStream {
         asm_calls += format!("\"{}\",", op).as_str();
     }
 
-    let block = format!("unsafe {{std::arch::asm!({})}}", asm_calls);
+    let block = format!("unsafe {{core::arch::asm!({})}}", asm_calls);
 
     println!("{}", block);
 
